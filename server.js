84266cadce30e4fbe298b85e347b5bbbcf90ab1d
Frederik De Bleser <frederik@debleser.be>
2018-05-21 14:43:45 +0200

    Use to PORT set in environment.

--- server.js
+++ server.js
@@ -41,4 +41,5 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
