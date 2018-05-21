01afd734b2a4f5b2db7d3dc22e9d6542cad84a25
Frederik De Bleser <frederik@debleser.be>
2018-05-21 14:40:01 +0200

    Update the docs.

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const HTML = `<!doctype html>
 <style>
 body { background: #f7f7f7; font: 14px sans-serif; color: #333; width: 900px; margin: 20px auto; }
 pre { margin: 10px 0; padding: 5px 10px; background: #eee; }
+a { text-decoration: none; color: azul; }
 </style>
 </head>
 <body><h1>Corset</h1>
 <p>Returns any request with <code>Access-Control-Allow-Origin: *</code> headers.</p>
+<p><small><a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS">Read more about Cross-Origin Resource Sharing.</a></small></p>
 <pre><code>https://corset.herokuapp.com/?url=https://example.com/</code></pre>
-<p>Use <code>encodeURIComponent</code> to encode the URL:</p>
+<p>Use <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent">encodeURIComponent</a> to encode the URL:</p>
 <pre><code>async function myFetch(url) {
     const wrappedUrl = 'https://corset.herokuapp.com/?url=' + encodeURIComponent(url);
     const res = await fetch(wrappedUrl);
@@ -31,9 +33,7 @@ app.get('/', (req, res) => {
         res.end(HTML);
         return;
     }
-
     const url = req.query.url;
-
     request.get(url, (proxyErr, proxyRes, proxyBody) => {
         res.status(proxyRes.statusCode);
         res.append('Access-Control-Allow-Origin', '*');
@@ -42,5 +42,3 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
-
-
